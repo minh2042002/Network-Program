8c4bfd2a3002947600577b4f2c776ec27b53e2f0
Trịnh Đức Minh <78490206+minh2042002@users.noreply.github.com>
2023-11-03 07:10:44 +0700

    Delete Week_2 directory

--- Week_2/service.h
+++
@@ -1,9 +0,0 @@
-#ifndef SERVICE_H
-#define SERVICE_H
-
-void findIPAddressByDomainName(char *domainName);
-void findDomainNameByIPAddress(char *ipAddress);
-int is_valid_ipv4(const char *input);
-int is_valid_domain(const char *input);
-
-#endif
